133cc37f740450e53d64841447e3074aca098c23
Dinesh Singh <dinesh2424@gmail.com>
2023-03-12 00:00:12 -0800

    Working transform with hardcoded jest transform config options
    Need to pass ts-jest config to getTsJestTransformer

--- transformer/barrelImportTransformerProcessor.ts
+++ transformer/barrelImportTransformerProcessor.ts
@@ -1,14 +1,33 @@
 import * as ts from "typescript";
+import { createHash } from 'crypto';
+import * as tsJest from 'ts-jest';
 import { barrelImportTransformer } from "./barrelImportTransformer";
+import type {
+    TransformOptions as JestTransformOptions,
+    SyncTransformer,
+    TransformerCreator,
+} from '@jest/transform';
+
+const getTsJestTranformer = () => {
+    return tsJest.default.createTransformer({
+        isolatedModules: true
+    });
+}
 
 /**
  * Processes the file by transforming the barrel imports using TSC Api as in barrelImportTransformer.ts file.
  * Then the transformed file is passed to the ts-jest to process it further
  */
-export const process = (sourceText: string, sourcePath: string, config: any, options: any) => {
+const transformWithTsJest = (sourceText: string, sourcePath: string, config: tsJest.TransformOptionsTsJest) => {
     printLog("->Transforming file -", sourcePath);
     const transformedText = getTransformedText(sourcePath, sourceText);
-    return require("ts-jest").createTransformer().process(transformedText, sourcePath, config, options);
+    return getTsJestTranformer().process(transformedText, sourcePath, config);
+};
+
+const transformWithTsJestAsync = (sourceText: string, sourcePath: string, config: tsJest.TransformOptionsTsJest) => {
+    printLog("->Transforming file async -", sourcePath);
+    const transformedText = getTransformedText(sourcePath, sourceText);
+    return getTsJestTranformer().processAsync(transformedText, sourcePath, config);
 };
 
 const printLog = (...args: any[]) => {
@@ -31,3 +50,94 @@ const getTransformedText = (sourcePath: string, sourceText: string) => {
     printLog(" ->Transformed Source -", newSourceText);
     return newSourceText;
 };
+
+export const name = 'BarrelTransformer';
+export const version = "1.0.0";
+
+function getCacheKeyFromConfig(
+    sourceText: string,
+    sourcePath: string,
+    transformOptions: JestTransformOptions,
+): string {
+    const { config, configString, instrument } = transformOptions;
+
+    return createHash('sha1')
+        .update(sourceText)
+        .update('\0', 'utf8')
+        .update(sourcePath)
+        .update('\0', 'utf8')
+        .update(JSON.stringify(config ?? ''))
+        .update('\0', 'utf8')
+        .update(configString)
+        .update('\0', 'utf8')
+        .update(instrument ? 'instrument' : '')
+        .update('\0', 'utf8')
+        .update(process.env.NODE_ENV ?? '')
+        .update('\0', 'utf8')
+        .update(process.env.BABEL_ENV ?? '')
+        .update('\0', 'utf8')
+        .update(process.version)
+        .digest('hex')
+        .substring(0, 32);
+}
+
+export const createTransformer: TransformerCreator<
+    SyncTransformer<JestTransformOptions>,
+    JestTransformOptions
+> = (_userOptions) => {
+    return {
+        canInstrument: true,
+        getCacheKey(sourceText, sourcePath, transformOptions) {
+            const cacheKey = getCacheKeyFromConfig(
+                sourceText,
+                sourcePath,
+                transformOptions,
+            );
+
+            printLog('Cache key for - ', sourcePath, '-', cacheKey);
+            return cacheKey;
+        },
+        async getCacheKeyAsync(sourceText, sourcePath, transformOptions) {
+            const cacheKey = getCacheKeyFromConfig(
+                sourceText,
+                sourcePath,
+                transformOptions,
+            );
+
+            printLog('Cache key async for - ', sourcePath, '-', cacheKey);
+            return cacheKey;
+        },
+        process(sourceText, sourcePath, transformOptions) {
+            const transformResult = transformWithTsJest(sourceText, sourcePath, transformOptions);
+
+            if (transformResult) {
+                const { code, map } = transformResult;
+                if (typeof code === 'string') {
+                    return { code, map };
+                }
+            }
+
+
+            return { code: sourceText };
+        },
+        async processAsync(sourceText, sourcePath, transformOptions) {
+            const transformResult = await transformWithTsJestAsync(sourceText, sourcePath, transformOptions);
+            if (transformResult) {
+                const { code, map } = transformResult;
+                if (typeof code === 'string') {
+                    return { code, map };
+                }
+            }
+
+            return { code: sourceText };
+        },
+    };
+};
+
+const transformerFactory = {
+    // Assigned here, instead of as a separate export, due to limitations in Jest's
+    // requireOrImportModule, requiring all exports to be on the `default` export
+    createTransformer
+};
+
+export default transformerFactory;
